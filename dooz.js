1e6ae5d0af06a8fa337460bc42dce3643f835235
Ehsan_Heidari <ehsan.heydari@gmail.com>
2020-07-25 19:12:13 -0700

    final commit

--- dooz.js
+++ dooz.js
@@ -1,4 +1,5 @@
-var click = 0;
+const size = 3;
+let click = 0;
 function procces() {
     click += 1;
     var rowNumber = document.getElementById("row").value;
@@ -12,5 +13,21 @@ function procces() {
         document.getElementById("table1").rows[rowNumber - 1].cells[columnNumber - 1].innerHTML = "O";
     }
     if (click > 4) {
+        for (let i = 0; i < size; i++) {
+            const iterator = row[i].value()
+            for (const value of iterator)
+                if (value == X | O) {
+                    document.getElementById(winer).innerHTML = "You are winer..."
+                }
+
+        }
     }
 }
+//     let array = []
+//     rowindex = array.slice(0, size - 1)
+//     array.push[i]
+//     if (item == X | O) {
+//         document.getElementById(winer).innerHTML = "You are winer..."
+
+//     }
+// }
